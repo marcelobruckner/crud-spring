f6be1a06b893a9cb4db7d289ab812eb13faa7bff
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-06-09 13:19:09 -0300

    Ajuste em imports

--- src/main/java/com/bruckner/controller/CourseController.java
+++ src/main/java/com/bruckner/controller/CourseController.java
@@ -1,13 +1,12 @@
 package com.bruckner.controller;
 
-import java.util.List;
-
 import com.bruckner.model.Course;
 import com.bruckner.repository.CourseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
