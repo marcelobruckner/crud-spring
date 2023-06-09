798bf173d88d615eaf3c42802eb05a5c223202c1
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-06-09 13:11:15 -0300

    primeiro commit

---
+++ src/main/java/com/bruckner/CrudSpringApplication.java
@@ -0,0 +1,28 @@
+package com.bruckner;
+
+import com.bruckner.model.Course;
+import com.bruckner.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course course = new Course();
+			course.setName("Angular com Spring");
+			course.setCategory("front-end");
+			courseRepository.save(course);
+		};
+	}
+}
---
+++ src/main/java/com/bruckner/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.bruckner.repository;
+
+import com.bruckner.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
