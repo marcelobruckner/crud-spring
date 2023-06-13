917247cb72742ab5b1957ccf3390a62c9c6268a5
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-06-13 13:44:15 -0300

    Implementando a criação de cursos.

--- src/main/java/com/bruckner/controller/CourseController.java
+++ src/main/java/com/bruckner/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.bruckner.controller;
 import com.bruckner.model.Course;
 import com.bruckner.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +20,11 @@ public class CourseController {
   public List<Course> list() {
     return courseRepository.findAll();
   }
+
+  @PostMapping
+  public ResponseEntity<Course> create(@RequestBody Course course){
+    return ResponseEntity.status(HttpStatus.CREATED)
+            .body(courseRepository.save(course));
+  }
 }
+
