498a3319fc2dda3036a3a66bdc65f4f65b57b5bc
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-06-16 21:13:42 -0300

    Implementando a edição de curso.

--- src/main/java/com/bruckner/controller/CourseController.java
+++ src/main/java/com/bruckner/controller/CourseController.java
@@ -1,14 +1,21 @@
 package com.bruckner.controller;
 
-import com.bruckner.model.Course;
-import com.bruckner.repository.CourseRepository;
-import lombok.AllArgsConstructor;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.bruckner.model.Course;
+import com.bruckner.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -23,16 +30,27 @@ public class CourseController {
   }
 
   @PostMapping
-  public ResponseEntity<Course> create(@RequestBody Course course){
+  public ResponseEntity<Course> create(@RequestBody Course course) {
     return ResponseEntity.status(HttpStatus.CREATED)
-            .body(courseRepository.save(course));
+        .body(courseRepository.save(course));
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<Course> findById(@PathVariable Long id) {
     return courseRepository.findById(id)
-           .map(course -> ResponseEntity.ok().body(course))
-           .orElse(ResponseEntity.notFound().build());
+        .map(recordFound -> ResponseEntity.ok().body(recordFound))
+        .orElse(ResponseEntity.notFound().build());
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+    return courseRepository.findById(id)
+        .map(recordFound -> {
+          recordFound.setName(course.getName());
+          recordFound.setCategory(course.getCategory());
+          Course updated = courseRepository.save(recordFound);
+          return ResponseEntity.ok().body(updated);
+        })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
-
